69abcfcf7902ace62c92362b1a4d737e4852c76d
Calore, Federico <Federico.Calore@nike.com>
2017-05-17 00:43:30 +0200

    Completed unit tests

--- tests/testthat/test_FARS_functions.R
+++ tests/testthat/test_FARS_functions.R
@@ -1,5 +1,13 @@
 test_that("Fars function tests", {
 
+  # make_filename()
   expect_equal(make_filename(2013), "accident_2013.csv.bz2")
+
+  # fars_read()
+  datafile <- system.file("extdata", "accident_2013.csv.bz2", package = "buildRpackage")
+
+  expect_equal(nrow(fars_read(datafile)), 30202) # check number of rows
+  expect_equal(sum(fars_read(datafile)$MONTH), 203624) # check sum of columns
+  expect_equal(unique(fars_read(datafile)$YEAR), 2013) # check year
 
 })
